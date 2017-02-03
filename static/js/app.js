181c09aed40ec4f76bda7e8a493473b4936e4a93
jaf7 <janthonyfields@gmail.com>
2017-02-02 18:47:07 -0700

    Add Waypoints scrolling, rearrange app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,10 @@
 /*-------------------------------------------------------------------
 Wrapping in IIFE (advisable?)
+Experimenting without IIFE
 -------------------------------------------------------------------*/
 // (function($, document, window, viewport) {
-  var viewport = ResponsiveBootstrapToolkit;
   $(document).ready(function() {
+    var viewport = ResponsiveBootstrapToolkit;
 
     // Arguments to pass to plot_growth() in routes.py
     var plotData = {
@@ -11,14 +12,17 @@ Wrapping in IIFE (advisable?)
       upperLimit: 1000,
       plotWidth: 8,
       plotHeight: 7,
-      compareType: "placeHolder"
+      compareType: null // if still null, no calls to genPlot()
     };
+    var plotChosen = plotData.compareType;
 
 
     /*
-      mpld3 figures are not responsive: Connect appropriate
-      matplotlib figure size arguments to bootstrap viewport
-      breakpoints using Responsive Bootstrap Toolkit.
+      mpld3 figures are not responsive: On document.ready(),
+      pass appropriate matplotlib Figure instance arguments
+      (figsize=[tuple], w,h in inches) relative to bootstrap
+      viewport breakpoints, using Responsive Bootstrap Toolkit .is()
+      method.
       How to avoid duplicating this code? (see viewportChanged below)
     */
     if (viewport.is('<=sm')) {
@@ -36,20 +40,95 @@ Wrapping in IIFE (advisable?)
 
 
     /*
+      mpld3 figures are not responsive:
+      window resize events should trigger re-drawing figure at appropriate
+      dimensions. debounce() prevents multiple calls to plot_growth() and
+      multiple mpld3 renderings. It will not trigger until it hasn't been
+      invoked in [wait] milliseconds. [immediate] triggers on the leading
+      edge instead of the trailing edge. This debounce() is adapted from
+      Underscore.js and https://davidwalsh.name/javascript-debounce-function.
+    */
+    var debounce = function(myFunction, wait, immediate) {
+      var timeout;
+      return function() {
+        var context = this,
+          args = arguments;                   // store the correct references to pass into closures
+        var callNow = immediate && !timeout;  // conditions for leading-edge firing
+        var later = function() {              // to be fired on trailing edge of wait time
+          timeout = null;                     // fired, reset to null so callNow is an option again
+          if (!immediate) { myFunction.apply(context, args); }  // use apply() to pass in context of calling object (this)
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);    // why does this get invoked though it's stored in a variable? <--??
+        if (callNow) { myFunction.apply(context, args); }
+      };
+    };
+    /*
+      The function to pass to debounce(), and fire rate limit.
+      How to avoid duplicating this code? (Can't pass a
+      reference into debounce() inner function scopes, so
+      conditional code is repeated here-- how to avoid this?)
+    */
+    var viewportChanged = debounce(function() {
+      if (viewport.is('<=sm')) {
+        plotData.plotWidth = 6;
+        plotData.plotHeight = 5;
+      }
+      else if (viewport.is('md')) {
+        plotData.plotWidth = 6;
+        plotData.plotHeight = 5;
+      }
+      else if (viewport.is('>md')) {
+        plotData.plotWidth = 7;
+        plotData.plotHeight = 6;
+      }
+      genPlot();
+    }, 500);
+    // On resize, call viewportChanged
+    window.addEventListener("resize", function() {
+      if ( plotChosen ) { viewportChanged(); }
+    });
+
+
+    /*
+      Removed buttons temporarily (relocate)
       Handle click events on buttons. Use jQuery .on() method
       to filter the child element. This restricts the classList to
       the object existing before MathJax class insertions.
     */
-    $(".btn-flex-container").on('click', '.btn-plot-choice', function() {
-      var compareChoice = this.classList[0];
-      plotData.compareType = compareChoice;
-      genPlot();
-    });
+    // $(".btn-flex-container").on('click', '.btn-plot-choice', function() {
+    //   var compareChoice = this.classList[0];
+    //   plotData.compareType = compareChoice;
+    //   genPlot();
+    // });
+
 
     /*
-      Slider creation https://refreshless.com/nouislider
+      Waypoints function to trigger on scrolling to
+      each .growthDescription element
+      -- add debounce()
+      -- refine: selectively trigger (for ex. don't
+         trigger when scrolling back up to last element)
+    */
+    var scrollWaypoints = $( ".growthDescription" ).waypoint(
+      function() {
+        console.log( "plotType", plotType);
+        console.dir(this);
+
+        var plotType = this.element.id;
+        plotData.compareType = plotType;
+        genPlot();
+      },
+      { offset: "60%" }
+    );
+
+
+    /*
+      Slider creation, binding, styling
+      https://refreshless.com/nouislider
       (no jQuery)
     */
+    // -- Create the slider
     var limitSlider = document.getElementById("limit-slider"),
         lowerLimitInput = document.getElementById("lower-limit-input"),
         upperLimitInput = document.getElementById("upper-limit-input");
@@ -110,54 +189,6 @@ Wrapping in IIFE (advisable?)
 
 
     /*
-      mpld3 figures are not responsive:
-      window resize events should trigger re-drawing figure at appropriate
-      dimensions. debounce() prevents multiple calls to plot_growth() and
-      multiple mpld3 renderings. It will not trigger until it hasn't been
-      invoked in [wait] milliseconds. [immediate] triggers on the leading
-      edge instead of the trailing edge. This debounce() is adapted from
-      Underscore.js and https://davidwalsh.name/javascript-debounce-function.
-    */
-    var debounce = function(myFunction, wait, immediate) {
-      var timeout;
-      return function() {
-        var context = this,
-          args = arguments;                   // store the correct references to pass into closures
-        var callNow = immediate && !timeout;  // conditions for leading-edge firing
-        var later = function() {              // to be fired on trailing edge of wait time
-          timeout = null;                     // fired, reset to null so callNow is an option again
-          if (!immediate) { myFunction.apply(context, args); }  // use apply() to pass in context of calling object (this)
-        };
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);    // why does this get invoked though it's stored in a variable? <--??
-        if (callNow) { myFunction.apply(context, args); }
-      };
-    };
-    /*
-      The function to execute and fire rate limit.
-      How to avoid duplicating this code? (Can't pass a
-      reference into debounce() inner function scopes)
-    */
-    var viewportChanged = debounce(function() {
-      if (viewport.is('<=sm')) {
-        plotData.plotWidth = 6;
-        plotData.plotHeight = 5;
-      }
-      else if (viewport.is('md')) {
-        plotData.plotWidth = 6;
-        plotData.plotHeight = 5;
-      }
-      else if (viewport.is('>md')) {
-        plotData.plotWidth = 7;
-        plotData.plotHeight = 6;
-      }
-      genPlot();
-    }, 500);
-    // On resize, pass to debounce() the function to execute
-    window.addEventListener("resize", viewportChanged);
-
-
-    /*
       $.ajax() method wrapper: submit updated state to plot_growth() in routes.py
       (why doesn't it work to call getPlotSlide directly?) <--??
       Assign $.ajax() method to a variable in order to use .done()
@@ -172,22 +203,15 @@ Wrapping in IIFE (advisable?)
         dataType: "html"
       });
       getPlotSlide.done(function(data) {
-        $("#plot-space").fadeTo(500, 0, function() {
-          $("#plot-space").html(data).fadeTo(500, 1);
+        $("#plot-space").fadeTo(200, 0, function() {
+          $("#plot-space").html(data).fadeTo(200, 1);
         });
-        $("#show-text").fadeTo(500, 0, function() {
-          $(this).html(descriptions[plotData.compareType]).fadeTo(500, 1, function() {});
-        });
+        // $("#show-text").fadeTo(500, 0, function() {
+        //   $(this).html(descriptions[plotData.compareType]).fadeTo(500, 1, function() {});
+        // });
       });
     };
 
-
-    var descriptions = {
-      "linearLog": "<p>linearLog description</p>",
-      "linearLogLinear": "linearLogLinear description",
-      "logLinearQuadratic": "logLinearQuadratic description",
-      "plotDataadraticExponential": "plotDataadraticExponential description"
-    };
 
   });
 // })(jQuery, document, window, ResponsiveBootstrapToolkit);
